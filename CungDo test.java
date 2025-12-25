71dc426eafd501ba31428c50a43cf0a5f11009eb
CungDo <corndogcungdo@gmail.com>
2025-12-25 10:36:26 +0100

    Cung Do Java test, VScode does not understand it tho :(

---
+++ CungDo test.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="CungDotest");
+public class CungDotest extends LinearOpMode{
+    private Dcmotor motorLeft;
+    private Dcmotor motorRight;
+
+    public void runOpMode() {
+        motorLeft = hardwaremap.get(Dcmotor.class, "motorLeft");
+        motorLeft = hardwaremap.get(Dcmotor.class, "motorRight");
+        motorLeft.setDirection(Dcmotor.Direction.REVERSE);
+        waitForStart;
+        while (opModeIsActive()){
+            double left = -gamepad1.left_stick_y;
+                double right = -gamepad1.right_stick_y;
+
+                motorLeft.setPower(left);
+                motorRight.setPower(right);
+            }
+        }
+}
